d5035c9b8c2af627fbc2426c46e4bc3f2674bbb2
Alison Park <alisonpark@Alisons-MacBook-Pro-2.local>
2022-04-09 09:13:53 -0500

    modularize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { hasConflict, timeConflict, terms } from './utilities/times.js';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -57,9 +58,6 @@ const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
-const days = ['M', 'Tu', 'W', 'Th', 'F'];
-
-
 
 
 const CourseList = ({ courses }) => {
@@ -86,7 +84,7 @@ const toggle = (x, lst) => (
   lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
 
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
@@ -115,9 +113,7 @@ const Course = ({ course, selected, setSelected }) => {
   );
 };
 
-const hasConflict = (course, selected) => (
-  selected.some(selection => courseConflict(course, selection))
-);
+
 
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
@@ -156,6 +152,7 @@ const TermButton = ({term, setTerm, checked}) => (
   </>
 );
 
+
 const TermSelector = ({term, setTerm}) => (
   <div className="btn-group">
   { 
@@ -164,18 +161,6 @@ const TermSelector = ({term, setTerm}) => (
     ))
   }
   </div>
-);
-
-const daysOverlap = (days1, days2) => ( 
-  days.some(day => days1.includes(day) && days2.includes(day))
-);
-
-const hoursOverlap = (hours1, hours2) => (
-  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
-);
-
-const timeConflict = (course1, course2) => (
-  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
 );
 
 const courseConflict = (course1, course2) => (
